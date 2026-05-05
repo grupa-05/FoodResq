653b43b372cacfafaf5f754f6a7add6e4662340b
mihai <stopmihai17@gmail.com>
2026-05-06 01:23:26 +0300

    add loading-error-path page example on route /loading-error-path

---
+++ src/app/loading-error-path/page.js
@@ -0,0 +1,6 @@
+export default async function CustomPath()
+{
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    throw new Error("test error");
+    return <div> Custom Path</div>
+}
